ee7f20ed0b0df6ad5c1c2beb49ec2dc051d10006
GrzegorzDryja <GrzegorzDryja.via@gmail.com>
2020-10-19 21:39:58 +0200

    CRUD

--- ClientListManager/Startup.cs
+++ ClientListManager/Startup.cs
@@ -1,3 +1,5 @@
+using ClientListManager.Data;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,12 @@ namespace ClientListManager
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+/*            services.AddDbContext<ClientContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("ClientContext")));*/
+
+            services.AddDbContext<ClientListManagerContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ClientListManagerContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
