b4e03a249220a4222f11f9b6c025176d2d5106cd
GrzegorzDryja <GrzegorzDryja.via@gmail.com>
2020-10-19 21:00:14 +0200

    New setup

--- ClientListManager/Startup.cs
+++ ClientListManager/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,8 +8,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
-using ClientListManager.Data;
 
 namespace ClientListManager
 {
@@ -26,9 +24,6 @@ namespace ClientListManager
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-
-            services.AddDbContext<ClientListManagerContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("ClientListManagerContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
