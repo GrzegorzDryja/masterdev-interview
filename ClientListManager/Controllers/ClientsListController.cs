07737469dddf60980b70ca6d1d0951bf829e963b
GrzegorzDryja <GrzegorzDryja.via@gmail.com>
2020-10-20 16:20:41 +0200

    Almost working

--- ClientListManager/Controllers/ClientsListController.cs
+++ ClientListManager/Controllers/ClientsListController.cs
@@ -7,6 +7,12 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ClientListManager.Data;
 using ClientListManager.Models;
+using CsvHelper;
+using Newtonsoft.Json;
+using System.IO;
+using System.Globalization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
 
 namespace ClientListManager.Controllers
 {
@@ -148,6 +154,46 @@ namespace ClientListManager.Controllers
         private bool ClientExists(int id)
         {
             return _context.Client.Any(e => e.Id == id);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file, [FromServices] IHostEnvironment hostEnvironment)
+        {
+            string fileName = $"{hostEnvironment.ContentRootPath}\\wwwroot\\files\\{file.FileName}";
+            using (FileStream fileStream = System.IO.File.Create(fileName))
+            {
+                file.CopyTo(fileStream);
+                fileStream.Flush();
+            }
+
+            GetClientList(file.FileName);
+
+            return View(await _context.Client.ToListAsync());
+        }
+
+        private async void GetClientList(string fName)
+        {
+            var fileName = $"{Directory.GetCurrentDirectory()}{@"\wwwroot\files"}" + "\\" + fName;
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            using (var stream = System.IO.File.Open(fileName, FileMode.Open, FileAccess.Read))
+            {
+                using var reader = new StreamReader(stream);
+                using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+                csv.Configuration.HeaderValidated = null;
+                csv.Configuration.MissingFieldFound = null;
+                var records = csv.GetRecords<Client>();
+
+                //csv.GetRecords<Client>().ToList().Dump();
+
+                foreach (var item in records)
+                {
+                    _context.Add(item);                    
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            System.IO.File.Delete(fileName);
+
         }
     }
 }
